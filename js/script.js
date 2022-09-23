6eed004b0aa789af31c7b0a22fd68622a12d29a5
hiago640 <hiago_pereiraborges@hotmail.com>
2022-09-23 17:02:22 -0300

    feat: preencher quadrados ao digitar

--- js/script.js
+++ js/script.js
@@ -1,56 +1,100 @@
-const grid = document.getElementById('grid')
-const input = document.getElementById('input')
+const grid = document.getElementById("grid");
+const input = document.getElementById("input");
 
-const MAX_NUMBER_ROWS = 6
-const MAX_NUMBER_CELL = 5
+const MAX_NUMBER_ROWS = 6;
+const MAX_NUMBER_CELL = 5;
+
+let attempt = 1;
 
 function initGrid() {
-
 	for (let rowIndex = 1; rowIndex <= MAX_NUMBER_ROWS; rowIndex++) {
-		let row = document.createElement('div')
-		row.setAttribute('class', 'row')	
-		row.setAttribute('id', `row-${rowIndex}`)
-		
-		grid.appendChild(row)
+		let row = document.createElement("div");
+		row.setAttribute("class", "row");
+		row.setAttribute("id", `row-${rowIndex}`);
 
-		createCell(row, rowIndex)
+		grid.appendChild(row);
+
+		createCell(row, rowIndex);
 	}
-
 }
 
 function createCell(row, rowIndex) {
-
 	for (let i = 1; i <= MAX_NUMBER_CELL; i++) {
-		let cell = document.createElement('div')
-		cell.setAttribute('class', 'cell')	
-		cell.setAttribute('id', `cell-${rowIndex}-${i}`)	
-		
-		if(rowIndex != 1)
-			cell.setAttribute('class', 'cell inative')
+		let cell = document.createElement("div");
+		cell.setAttribute("class", "cell");
+		cell.setAttribute("id", `cell-${rowIndex}-${i}`);
 
-		row.appendChild(cell)
+		if (rowIndex != 1) cell.setAttribute("class", "cell inative");
+
+		row.appendChild(cell);
 	}
-	
 }
 
-initGrid()
+initGrid();
+let nextLetter = 0;
 
-function mostra() {
-	let inputWord = input.value.toUpperCase()
+function insertLetter(letter) {
+	let row = document.getElementById(`row-${attempt}`);
+	console.log("entro");
 
-	for (let attempts = 1; attempts <= MAX_NUMBER_ROWS; attempts++) {
-		let row = document.getElementById(`row-${attempts}`)
-		
-		if(attempts == 1){
-			for(let index = 1; index <= MAX_NUMBER_CELL; index++){
-				
-				row.childNodes[index-1].textContent = inputWord[index-1]
-				
-			}
+	let cell = row.childNodes[nextLetter];
+	cell.textContent = letter;
+	nextLetter++;
+}
 
+/*
+function insertLetter(keyPressed) {
+	if (nextLetter === MAX_NUMBER_CELL) {
+		attempt++;
+		nextLetter = 0;
+	}
+
+	if (attempt > MAX_NUMBER_ROWS) return;
+
+	let row = document.getElementById(`row-${attempt}`);
+	let cell;
+
+	if (keyPressed == "Backspace" || keyPressed == "Delete") {
+		cell = row.childNodes[nextLetter].classList.remove("ative");
+
+		if (nextLetter != 0) {
+			nextLetter--;
+			cell = row.childNodes[nextLetter];
+			cell.textContent = "";
+			cell.classList.add("ative");
+		}
+	} else {
+		cell = row.childNodes[nextLetter];
+		cell.classList.add("ative");
+		row.childNodes[nextLetter].textContent = keyPressed.toUpperCase();
+		//cell.classList.remove('ative')
+		nextLetter++;
+	}
+}*/
+document.addEventListener("keyup", (e) => {
+	
+		if (attempt === 7) {
+			return;
 		}
 
-		
-	}
-	input.value = ""
-}
+		let pressedKey = String(e.key)
+
+		if (pressedKey === "Enter") {
+			//checkGuess()
+			return;
+		}
+
+		if (pressedKey === "Backspace") {
+			//deleteLetter()
+			return;
+		}
+
+		let found = pressedKey.match(/[a-z]/gi)
+		if (!found || found.length > 1) {
+			return
+		} else {
+			insertLetter(pressedKey)
+		}
+	},
+	false
+);
